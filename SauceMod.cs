ec330ae1625ebac727bd21739fde8e8dbd61cb16
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2019-01-05 15:52:51 -0500

    Update SauceMod.cs

--- SauceMod.cs
+++ SauceMod.cs
@@ -1,19 +1,35 @@
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.GameContent.Dyes;
+using Terraria.GameContent.UI;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.UI;
+
 
 namespace SauceMod
 {
-	class SauceMod : Mod
-	{
-		public SauceMod()
-		{
-			properties = new ModProperties()
-            		{
-                		Autoload = true,
-                		AutoloadGores = true,
-                		AutoloadSounds = true,
-                		AutoloadBackgrounds = true
-            		};
-            		instance = this;
-		}
-	}
+    class SauceMod : Mod
+    {
+        internal static SauceMod instance;
+
+        public SauceMod()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true,
+                AutoloadBackgrounds = true
+            };
+            instance = this;
+        }
+    }
 }
